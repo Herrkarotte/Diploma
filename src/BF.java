78aac9876247549a3d9f99b3dc45697f2a1eb0a0
HerrKarotte <dvp03ne@yandex.ru>
2025-03-15 23:09:15 +0300

    Добавлены поясняющие комментарии.

--- src/BF.java
+++ src/BF.java
@@ -16,36 +16,56 @@ import java.util.concurrent.atomic.AtomicInteger;
  * после чего происходит поиск максимального значения критерия среди перестановок внутри задачи,
  * а так же среди всех задач.
  *
+ * Метод generatePermutation():
+ * получает задачу и очередь. Генерирует все возможные перестановки для задачи,
+ * после чего загружает список перестановок в очередь для дальнейшей обработки.
+ *
+ * Метод solveTask():
+ * получает список всех перестановок и производит вычисления критерия и поиск локального(для задачи)
+ * и глобального(среди всех задач) максимума критерия.
+ *
  * метод printResul():
  * производит печать полученного лучшего критерия и перестановки среди всех задач, а так же времени,
  * затраченного на выполнения решения всех задач.
+ *
+ * Количество доступных ядер процессора получается путем деления количества потоков путем деления на 2, подразумевая,
+ * что на каждое ядро процессора приходится 2 потока.
+ *
+ * Соотношение 84 к 16 было оптимальным на моей системе и было принято за основу для вычисления соотношения для
+ * других процессоров с другим количеством ядер/потоков. Например, для 12 ядер будет получено соотношение 10
+ * потоков-генераторов к 2 потокам-обработчикам, что может быть не самым оптимальным соотношением для такого процессора.
  * */
 
 public class BF {
+    // определяем доступное количество ядер на процессоре
+    int availableProcessors = (Runtime.getRuntime().availableProcessors()) / 2;
+    // количество потоков-генераторов и потоков-обработчиков в соотношении 84%к16%
+    int numOfGenerators = (int) (availableProcessors * 0.84f);
+    int numOfConsumers = availableProcessors - numOfGenerators;
 
-    int availableProcessors = (Runtime.getRuntime().availableProcessors())/2;
-
-    int listOfTaskSize;
+    // переменные для поиска лучшего решения среди всех выполненных задач
     private static int globalMaxF = 0;
-    long programStartTime = System.currentTimeMillis();
-
-    List<NIRData> tmpResult;
     private static List<NIRData> tmpGlobalResult;
+    //вспомогательные переменные
+    int listOfTaskSize;
+    long programStartTime = System.currentTimeMillis();
+    List<NIRData> tmpResult;
 
-    int numOfExecutors = (int)(availableProcessors*0.84f);
-    int numOfConsumers = availableProcessors-numOfExecutors;
 
     private static final AtomicInteger completedTasks = new AtomicInteger(0);
     List<List<NIRData>> POISON_PILL = new ArrayList<>();
 
     public void bfSolver(ArrayList<ArrayList<NIRData>> tasks) {
+
         listOfTaskSize = tasks.size();
+        //очередь для синхронизации потоков-генераторов и обработчиков
         BlockingDeque<List<List<NIRData>>> queue = new LinkedBlockingDeque<>(100);
-        ExecutorService generators = Executors.newFixedThreadPool(numOfExecutors);
+        //пулы потоков генераторов и обработчиков
+        ExecutorService generators = Executors.newFixedThreadPool(numOfGenerators);
         ExecutorService consumers = Executors.newFixedThreadPool(numOfConsumers);
-
+        // счетчик завершенных потоков обработчиков для корректного завершения выполнения метода
         CountDownLatch latch = new CountDownLatch(numOfConsumers);
-
+        // запуск потоков-генераторов
         for (ArrayList<NIRData> task : tasks) {
             generators.submit(() -> {
                 generatePermutation(task, queue);
@@ -60,6 +80,7 @@ public class BF {
                 }
             });
         }
+        // запуск потоков-обработчиков
         for (int i = 0; i < numOfConsumers; i++) {
             consumers.submit(() -> {
                 while (true) {
@@ -86,6 +107,7 @@ public class BF {
         }
     }
 
+    // генерация перестановок для задач.
     private void generatePermutation(ArrayList<NIRData> task, BlockingDeque<List<List<NIRData>>> queue) {
         try {
             PermutationIterator<NIRData> permutationIterator = new PermutationIterator<>(task);
@@ -96,15 +118,14 @@ public class BF {
         }
     }
 
+    //обработка перестановок.
     private void solveTask(List<List<NIRData>> permutations) {
         int startTime;
         int leadTime;
-
         int totalTime;
         int F;
         int maxF;
         maxF = 0;
-        F = 0;
         for (List<NIRData> permutation : permutations) {
             F = 0;
             totalTime = 0;
